9208a906e1d3dd726d5fc8dabf044c157664772b
Hikmatbek-dev <hikmatcoder@gmail.com>
2024-07-27 08:38:43 +0500

    Pythonda masalalar yechdim

---
+++ 1_dars.py
@@ -0,0 +1,63 @@
+# Begin 1 Perimetrni aniqlash
+# Kvadratni tomoni
+# a = float(input("Kvadratning tomonini kiriting va men Perimetrni aniqlab beraman: "))
+
+# # Perimetrni hisoblash
+# P = 4 * a
+
+# # Natijani chiqarish
+# print("Kvadratning perimetri:", P)
+
+#Begin 2 savol:uning yuzasi aniqlansin
+# # Kvadratning yuzasini aniqlash
+# b = float(input("Kvadratning tomonini kiriting va men yuzasini aniqlab beraman: "))
+
+
+# # Yuzasini hisoblash
+# S = b ** 2
+
+# # Natijani chiqarish
+# print("Kvadratning yuzasi:", S)
+
+#Begin 3 savol:uning yuzasi va primeteri aniqlansin
+# eni = float(input("Kvadratning enining razmerini kiriting: "))
+# boyi = float(input("Kvadratning enining razmerini kiriting: "))
+
+# print("Kiritilgan kvadratning yuzasi:", eni * boyi)
+# print("Kiritilgan kvadratning perimetri:", 2 * (eni + boyi))
+
+# #Begin 4 aylananing uzunligi aniqlansin
+# import math
+
+# # Aylanani diametri
+# d = float(input("Aylanani diametrini kiriting: "))
+
+# # Aylanani uzunligini hisoblash
+# L = math.pi * d
+
+# # Natijani chiqarish
+# print("Aylanani uzunligi:", L)
+
+# #Begin 5 savol:uning hajmi va sirti aniqlansin
+# h = float(input("Kubning yon tomon: "))
+# k =6*(h*h)
+# v = h*h*h
+# javob = print("hajmi" , v)
+# javob = print("sirti" , k)
+
+#Begin 6 savol:aylanasing uzunligi aniqlansin
+
+# o = float(input("Parlelepepedning tomonining hajmini kiriting: "))
+# p = float(input("Parlelepepedning tomonining hajmini kiriting: "))
+# q = float(input("Parlelepepedning tomonining hajmini kiriting: "))
+# hajmning_javobi = o * q * p
+# sirtning_javobi = 2*(o*p + p*q + o*q) 
+# javob = print("hajmi" , hajmning_javobi)
+# kebab = print("sirti" , sirtning_javobi)
+
+#Begin 7 uzunligi va yuzasi aniqlansin
+# R = float(input("Doiraning radusini kiriting: "))
+# L = 2 * 3.14 * R
+# Yuzasi_aylananing = 3.14 * (R * R)
+# jabob = print("Doiraning uzunligi " ,L )
+# jabob = print("Doiraning yuzasi " , Yuzasi_aylananing)
